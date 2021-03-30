78c6e785dea50aaf3344a234ba8dbc3087b07bee
raj-inovexia <rajkumar@innovagesoftwares.com>
2021-03-30 20:33:56 +0530

    Signed-off-by: raj-inovexia <rajkumar@innovagesoftwares.com>

--- assets/js/packing-pigeon.js
+++ assets/js/packing-pigeon.js
@@ -1,7 +1,40 @@
 (function ($) {
   $(document).ready(function () {
-    
+    new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      autoplay: {
+        delay: 2500,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 3,
+        },
+        768: {
+          slidesPerView: 4,
+        },
+        1024: {
+          slidesPerView: 5,
+        },
+      }
+    });
+
+    var turnround = new Swiper('.turnround', {
+      spaceBetween:0,
+      slidesPerView:1,
+      loopedSlides:1, //looped slides should be the same
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+     
+    });
+    var galleryTop = new Swiper('.video-frame', {
+      spaceBetween:0,
+      autoplay: {
+        delay: 3000,
+      },
+      loopedSlides:1, //looped slides should be the same
+      thumbs: {
+        swiper: turnround,
+      },
+    });
   });
 })(jQuery);
-
-
